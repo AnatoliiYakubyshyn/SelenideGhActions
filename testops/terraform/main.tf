4ed2c0ad93d3cbfee23172bc25877a7bf34c999c
Anatolii Yakubyshyn <anatoliiyakubyshyn@MacBook-Pro-Anatolii.local>
2024-07-12 19:07:33 +0300

    changed to pat

--- testops/terraform/main.tf
+++ testops/terraform/main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "runner" {
     sudo curl -o actions-runner-linux-x64-2.317.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.317.0/actions-runner-linux-x64-2.317.0.tar.gz
     sudo echo "9e883d210df8c6028aff475475a457d380353f9d01877d51cc01a17b2a91161d  actions-runner-linux-x64-2.317.0.tar.gz" | shasum -a 256 -c
     sudo tar xzf ./actions-runner-linux-x64-2.317.0.tar.gz
-    ./config.sh --url https://github.com/AnatoliiYakubyshyn/SelenideGhActions --token ${var.gh_token}
+    ./config.sh --url https://github.com/AnatoliiYakubyshyn/SelenideGhActions --pat ${var.gh_token}
     ./run.sh
   EOT
     interpreter = ["bash", "-c"]
