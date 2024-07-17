88325f94ca251b890aba0d69cd25eb485f1ef358
Anatolii Yakubyshyn <anatoliiyakubyshyn@MBP-Anatolii.bbrouter>
2024-07-17 13:50:59 +0300

    created scripts for provisioning

--- testops/provisioning/delete_script.py
+++ testops/provisioning/delete_script.py
@@ -4,19 +4,18 @@ import boto3.resources
 
 aws_access_key_id = sys.argv[1]
 aws_secret_access_key = sys.argv[2]
+ec2_id = sys.argv[3]
 
 instance_type = "t3.micro"
 ami = "ami-0249211c9916306f8"
 region = "eu-north-1"
 
 # Initialize a session using credentials
-ec2 = boto3.resource('ec2',
+ec2 = boto3.Session(
     aws_access_key_id=aws_access_key_id,
     aws_secret_access_key=aws_secret_access_key,
-    region_name=region
-)
+    region_name=region).client('ec2')
 
 # Create EC2 client
 
-response = ec2.create_instances(ImageId=ami,InstanceType=instance_type,MinCount=1,MaxCount=1)
-print(response[0].id)
+response = ec2.terminate_instances(InstanceIds=[ec2_id])
