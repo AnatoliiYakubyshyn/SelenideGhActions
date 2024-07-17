5e2381c67a5bd8400821870c55fce28b150bc9ce
Anatolii Yakubyshyn <anatoliiyakubyshyn@MBP-Anatolii.bbrouter>
2024-07-17 17:12:23 +0300

    fix

--- testops/provisioning/create_script.py
+++ testops/provisioning/create_script.py
@@ -21,16 +21,15 @@ ec2 = boto3.resource('ec2',
 
 user_data_script = f'''#!/bin/bash
 
-export AGENT_ALLOW_RUNASROOT="1"
 
 # Example user data script
 mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.317.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.317.0/actions-runner-linux-x64-2.317.0.tar.gz
 echo "9e883d210df8c6028aff475475a457d380353f9d01877d51cc01a17b2a91161d  actions-runner-linux-x64-2.317.0.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-linux-x64-2.317.0.tar.gz
-./config.sh --url https://github.com/AnatoliiYakubyshyn/SelenideGhActions --pat {gh_token}
-./svc.sh install
-./svc.sh start
+RUNNER_ALLOW_RUNASROOT=true ./config.sh --url https://github.com/AnatoliiYakubyshyn/SelenideGhActions --pat {gh_token}
+
+RUNNER_ALLOW_RUNASROOT=true ./run.sh
 '''
 # Create EC2 client
 
