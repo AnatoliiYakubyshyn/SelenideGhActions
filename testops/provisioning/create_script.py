8799471150c7c7dc1638ba0cc20503e7bfb84cd5
Anatolii Yakubyshyn <anatoliiyakubyshyn@MacBook-Pro-Anatolii.local>
2024-07-17 14:20:37 +0300

    implemented workflow file

--- testops/provisioning/create_script.py
+++ testops/provisioning/create_script.py
@@ -4,6 +4,7 @@ import boto3.resources
 
 aws_access_key_id = sys.argv[1]
 aws_secret_access_key = sys.argv[2]
+gh_token = sys.argv[3]
 
 instance_type = "t3.micro"
 ami = "ami-0249211c9916306f8"
@@ -16,7 +17,20 @@ ec2 = boto3.resource('ec2',
     region_name=region
 )
 
+
+user_data_script = f'''
+#!/bin/bash
+
+# Example user data script
+mkdir actions-runner && cd actions-runner
+curl -o actions-runner-linux-x64-2.317.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.317.0/actions-runner-linux-x64-2.317.0.tar.gz
+echo "9e883d210df8c6028aff475475a457d380353f9d01877d51cc01a17b2a91161d  actions-runner-linux-x64-2.317.0.tar.gz" | shasum -a 256 -c
+tar xzf ./actions-runner-linux-x64-2.317.0.tar.gz
+./config.sh --url https://github.com/AnatoliiYakubyshyn/SelenideGhActions --pat {gh_token}
+./run.sh
+'''
 # Create EC2 client
 
-response = ec2.create_instances(ImageId=ami,InstanceType=instance_type,MinCount=1,MaxCount=1)
+response = ec2.create_instances(ImageId=ami,InstanceType=instance_type,MinCount=1,MaxCount=1,
+                                UserData=user_data_script)
 print(response[0].id)
