5cab39c2234af8e4a9a879a817e634bc2cf06221
Anatolii Yakubyshyn <anatoliiyakubyshyn@MBP-Anatolii.bbrouter>
2024-07-19 13:07:00 +0300

    fixed bash script  by installing libicu

--- testops/provisioning/create_script.py
+++ testops/provisioning/create_script.py
@@ -21,13 +21,14 @@ ec2 = boto3.resource('ec2',
 
 user_data_script = f'''#!/bin/bash
 
-
 # Example user data script
 mkdir actions-runner && cd actions-runner
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 curl -o actions-runner-linux-x64-2.317.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.317.0/actions-runner-linux-x64-2.317.0.tar.gz
 echo "9e883d210df8c6028aff475475a457d380353f9d01877d51cc01a17b2a91161d  actions-runner-linux-x64-2.317.0.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-linux-x64-2.317.0.tar.gz
-RUNNER_ALLOW_RUNASROOT=true ./config.sh --url https://github.com/AnatoliiYakubyshyn/SelenideGhActions --pat {gh_token}
+yum install libicu -y
+RUNNER_ALLOW_RUNASROOT=true GITHUB_ACTIONS_RUNNER_HTTPTRACE=true ./config.sh --unattended --url https://github.com/AnatoliiYakubyshyn/SelenideGhActions --pat {gh_token}
 
 RUNNER_ALLOW_RUNASROOT=true ./run.sh
 '''
